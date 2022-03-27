5573570781f8ad83fa3bec9a6f7c541a944e68e7
José Ricardo <jricardobgoncalves@gmail.com>
2022-03-27 16:24:06 -0300

    Implementacao no terminal

--- Atividade 1/subscriber/index.js
+++ Atividade 1/subscriber/index.js
@@ -3,38 +3,38 @@ const express = require("express");
 const cors = require("cors");
 
 // bd
-const subscriber = require('./db/redis');
+const subscriber = require("./db/redis");
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.post('/subscribe', (req, res) => {
-    const {channel} = req.body;
+app.post("/subscribe", (req, res) => {
+  const { channel } = req.body;
 
-    subscriber.subscribe(channel);
-    res.send('Estou inscrito');
+  subscriber.subscribe(channel);
+  res.send("Estou inscrito");
 });
 
-app.delete('/subscribe', (req, res) => {
-    const {channel} = req.body;
+app.delete("/subscribe", (req, res) => {
+  const { channel } = req.body;
 
-    subscriber.unsubscribe(channel);
-    res.send('Inscrição Removida');
+  subscriber.unsubscribe(channel);
+  res.send("Inscrição Removida");
 });
 
-app.delete('/quit', (req, res) => {
-    subscriber.quit();
-    res.send('Quitando');
+app.delete("/quit", (req, res) => {
+  subscriber.quit();
+  res.send("Quitando");
 });
 
 subscriber.on("message", (channel, message) => {
-    // [HH:MM:SS] [<usuário>]: <mensagem>
-    console.log(message);
+  // [HH:MM:SS] [<usuário>]: <mensagem>
+  console.log(message);
 });
 
 app.listen(process.env.PORT, (err) => {
-    if(err) throw err;
-    console.log(`Server listening on port ${process.env.PORT}`);
+  if (err) throw err;
+  console.log(`Server listening on port ${process.env.PORT}`);
 });
